fc35dc865c0f8244862838fa7b62e4c7742a6c26
nordwaldberg <nord.waldberg@gmail.com>
2025-08-06 21:43:35 +0300

    Configure program

---
+++ src/index.js
@@ -0,0 +1,5 @@
+const genDiff = (filepath1, filepath2, format) => {
+  console.log(filepath1, filepath2, format)
+}
+
+export default genDiff
---
+++ src/parser/parsers.js
@@ -0,0 +1,5 @@
+import * as fs from 'node:fs'
+
+const parseJson = (data) => {
+
+}
